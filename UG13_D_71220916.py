6822781da39944f07e8aa98bfc9daf564005d0a0
sasalaoh10 <120088933+sasalaoh10@users.noreply.github.com>
2022-12-15 16:22:36 +0700

    Add files via upload

---
+++ UG13_D_71220916.py
@@ -0,0 +1,39 @@
+# ## Y
+import turtle
+t = turtle.Turtle()
+s = turtle.Screen()
+
+a = 1
+for i in range(a):
+    t.pensize(30)
+    t.pencolor("red")
+    t.left(115)
+    t.forward(150)
+    t.backward(150)
+    t.right(60)
+    t.forward(150)
+    t.backward(150)
+    t.right(145)
+    t.forward(150)
+    t.penup()
+    t.backward(150)
+    t.left(90)
+    t.pendown()
+
+t.up()
+
+t.forward(100)
+
+t.down()
+
+
+## L
+
+t.pensize(30)
+t.pencolor("blue")
+t.right(90)
+t.forward(170)
+t.left(90)
+t.forward(100)
+
+
